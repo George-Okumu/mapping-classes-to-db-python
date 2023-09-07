f5a138278ee5de8fc17da9f1fe72f76214764e6a
George-Okumu <gokumu12@gmail.com>
2023-09-07 22:12:34 +0300

    created table and called cursor to execute it

--- Song.py
+++ Song.py
@@ -2,6 +2,17 @@ from . import CONN, CURSOR
 
 class Song:
     def __init__(self, name, album):
+        self.id = None
         self.name = name
         self.album = album
 
+    @classmethod
+    def create_table(self):
+        sql = """
+                create table if not exists songs(
+                    id integer primary key,
+                    name varchar,
+                    album varcahr
+                    )
+            """
+        CURSOR.execute(sql)
